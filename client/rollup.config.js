3420523d055fcec92916b32fae2390f356a30d4a
Barney Boisvert <bboisvert@gmail.com>
2021-07-10 07:57:48 -0700

    add babel for ES translation

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 // `npm run build` -> `production` is true
@@ -16,6 +17,7 @@ export default {
 	plugins: [
 		resolve(), // tells Rollup how to find date-fns in node_modules
 		commonjs(), // converts date-fns to ES modules
+        babel({ babelHelpers: 'bundled' }),
 		production && terser() // minify, but only in production
 	]
 };
--- client/src/update.js
+++ client/src/update.js
@@ -2,7 +2,8 @@ import format from 'date-fns/format';
 
 var span = document.querySelector('#time-now');
 
-export default function update() {
-	span.textContent = format(new Date(), 'h:mm:ssa');
-	setTimeout(update, 1000);
-}
+const update = () => {
+    span.textContent = format(new Date(), 'h:mm:ssa');
+    setTimeout(update, 1000);
+};
+export default update
