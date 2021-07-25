647c0bb4045349ebaae516f314780a24966e8271
Barney Boisvert <bboisvert@gmail.com>
2021-07-25 13:34:35 -0700

    take the JSON off the homepage, and personalize greeting.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,25 +1,22 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import { auth } from "./firebase";
-import {
-    useUser,
-    useUserInfo,
-} from "./UserContext";
+import { useUser } from "./UserContext";
 
 function Home() {
     const user = useUser();
-    const [info] = useUserInfo();
+    const name = user?.displayName;
     return <React.Fragment>
+        <div>
+            <h2>Why Hello{name && `, ${name}`}!</h2>
+            <p>You look great, by the way. Very healthy.</p>
+        </div>
         {user && <Button variant={"contained"} color={"secondary"}
                          onClick={() => auth.signOut()}>
             SIGN OUT
         </Button>}
-        <div>
-            <h2>Why Hello!</h2>
-            <p>You look great, by the way. Very healthy.</p>
-        </div>
-        <pre>info: {JSON.stringify(info, null, 3)}</pre>
-        <pre>user: {JSON.stringify(user, null, 3)}</pre>
+        {/*<pre>info: {JSON.stringify(useUserInfo()[0], null, 3)}</pre>*/}
+        {/*<pre>user: {JSON.stringify(user, null, 3)}</pre>*/}
     </React.Fragment>;
 }
 
